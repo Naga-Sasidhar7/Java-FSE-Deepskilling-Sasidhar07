39ea0ee1606c2e2aab12d2695e26d8227dc5378a
Naga-Sasidhar7 <atchanagasasidhar@gmail.com>
2025-06-27 12:17:44 +0530

    Add files via upload

---
+++ Week2_Exercises/TDD-Mockito-SLF4J/TDD/Exercise-4/Calculator.java
@@ -0,0 +1,14 @@
+package TDD;
+
+public class Calculator2{
+    public int add(int a, int b) { return a + b; }
+
+    public int subtract(int a, int b) { return a - b; }
+
+    public int multiply(int a, int b) { return a * b; }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+}
